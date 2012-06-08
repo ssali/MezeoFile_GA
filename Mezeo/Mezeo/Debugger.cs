0576498d1c14c795487dbbccc9796f4c35964676
unknown <jwhite@mezeo.com>
2012-06-08 11:32:35 -0500

    Moved the log file from appdata\local into the appdata\local\application directory.

--- Mezeo/Mezeo/Debugger.cs
+++ Mezeo/Mezeo/Debugger.cs
@@ -19,7 +19,7 @@ namespace Mezeo
                 if (calssInstance == null)
                 {
                     calssInstance = new Debugger();
-                    debugFile = new FileStream( Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\" + "MezeoFileLogs.txt", FileMode.Append, FileAccess.Write);
+                    debugFile = new FileStream(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\" + AboutBox.AssemblyTitle + "\\" + AboutBox.AssemblyTitle  + ".log", FileMode.Append, FileAccess.Write);
                 }
 
                 return calssInstance;
